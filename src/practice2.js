12b3687e071004beb8c99750e57085fcd0590648
YumingChen <mathcym@outlook.com>
2020-11-09 23:02:08 +0800

    [yuming chen]: finish homework.

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,15 @@
-const inject
+const inject = (items, sections) => {
+  let output = [];
+  let pre = 0;
+  for (let i = 0; i < sections.length; ++i) {
+    let piece = items.slice(pre, sections[i].index);
+    output = output.concat(piece);
+    output.push(sections[i].content);
+    
+    pre = sections[i].index;
+  }
+  output = output.concat(items.slice(pre));
+  console.log(output);
+  return output;
+};
 export { inject };
